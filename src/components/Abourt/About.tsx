2f58df043c942069fb0a7a2113e58e621472c306
Shuliak Leonid <zeleny777@gmail.com>
2021-04-11 23:07:09 +0300

    fix block about

--- src/components/Abourt/About.tsx
+++ src/components/Abourt/About.tsx
@@ -5,24 +5,26 @@ const About = () => {
   return (
       <section className={s.wrapper}>
         <div className={s.blockAboutMe}>
-         <div className="title">About</div>
-           <div className="text">
-             <h3>Hello, I am Leonid Shuliak!</h3>
-             <p>
-               <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam dignissimos fuga fugiat fugit id ipsa iure magnam necessitatibus nihil nulla obcaecati officia officiis, omnis perspiciatis tempora tenetur totam voluptatibus!</span>
-             </p>
-             <p>
-               <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam dignissimos fuga fugiat fugit id ipsa iure magnam necessitatibus nihil nulla obcaecati officia officiis, omnis perspiciatis tempora tenetur totam voluptatibus!</span>
-             </p>
-             <p className={s.aboutMe}>
-               <span>Name: Leonid Shuliak</span>
-               <span>Age: 35</span>
-               <span>Address: Minsk,Belarus</span>
-               <span>Email: shuliak.leonid@tut.by</span></p>
-          <p>
-            <a href="#">Download CV</a>
-          </p>
-           </div>
+          <div className={s.title}>
+            About
+          </div>
+          <div className={s.text}>
+            <h3>Hello, I am Leonid Shuliak!</h3>
+            <p>
+              <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam dignissimos fuga fugiat fugit id ipsa iure magnam necessitatibus nihil nulla obcaecati officia officiis, omnis perspiciatis tempora tenetur totam voluptatibus!</span>
+            </p>
+            <p>
+              <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam dignissimos fuga fugiat fugit id ipsa iure magnam necessitatibus nihil nulla obcaecati officia officiis, omnis perspiciatis tempora tenetur totam voluptatibus!</span>
+            </p>
+            <p className={s.aboutMe}>
+              <span>Name: Leonid Shuliak</span>
+              <span>Age: 35</span>
+              <span>Address: Minsk,Belarus</span>
+              <span>Email: shuliak.leonid@tut.by</span></p>
+            <p>
+              <a download='' className={s.profile} href="https://www.coolfreecv.com/doc/resume_012.docx" title='Download CV'>Download CV</a>
+            </p>
+          </div>
         </div>
       </section>
   );
